b88e735628ed56d9b3a41cfa889777b1283c821f
coder7290 <topdev0729@gmail.com>
2021-11-07 22:54:16 +0100

    fix: merge with home2

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.png";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
 import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
-import Home2 from "./Home2";
 import Type from "./Type";
 
 function Home() {
@@ -19,7 +26,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> SOUMYAJIT BEHERA</strong>
+                <strong className="main-name"> Nan GuoXing</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
@@ -33,7 +40,101 @@ function Home() {
           </Row>
         </Container>
       </Container>
-      <Home2 />
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+              Making something that is &nbsp;
+              <b>visible to others</b>, <b>something that touches people</b>, <b>something that can be experienced</b>. <br />
+              I would like to introduce myself as an&nbsp;
+              <i><b>energetic person</b></i> who takes your business seriously, in an accessible and open way. <br />
+
+              After work experience in IT, I have developed many modern-style websites using modern  <b>JavaScript libraries</b>  like <b className="purple">React, Meteor</b>. <br />
+              <i><b>My favorite part about web design</b></i> &nbsp; is building a solution that&nbsp;<b className="purple">impresses the client</b> and <b className="purple">meets the needs of users and customers</b>. <br />
+
+              Also, I have teamwork traits including <i><b className="purple">reliability</b></i>, <i><b className="purple">good communication</b></i>, <i><b className="purple">commitment</b></i>, <i><b className="purple">adaptability</b></i>. <br />
+              <br />
+              Professional Experiences: <br />
+              &nbsp;&nbsp; Frontend: <br />
+              &nbsp;&nbsp;&nbsp;&nbsp;- HTML, CSS, CSS frameworks like Bootstrap, Semantic UI. <br />
+              &nbsp;&nbsp;&nbsp;&nbsp;- JavaScrip, JQuery, React, Angular, Meteor js, Gatsby js, Vue js. <br />
+              &nbsp;&nbsp; Backend: <br />
+              &nbsp;&nbsp;&nbsp;&nbsp;- Node js, Express js, Next js. <br />
+              &nbsp;&nbsp;&nbsp;&nbsp;- Django, Django Rest Framework, Flask. <br />
+              &nbsp;&nbsp; DataBase: <br />
+              &nbsp;&nbsp;&nbsp;&nbsp;- MongoDB <br />
+              &nbsp;&nbsp;&nbsp;&nbsp;- Google Firebase <br />
+              &nbsp;&nbsp;&nbsp;&nbsp;- MySQL <br />
+              &nbsp;&nbsp;&nbsp;&nbsp;- Redis <br />
+              &nbsp;&nbsp;&nbsp;&nbsp;- PostgreSQL <br />
+              &nbsp;&nbsp; API: <br />
+              &nbsp;&nbsp;&nbsp;&nbsp;-REST API <br />
+              &nbsp;&nbsp;&nbsp;&nbsp;-Twilio, Vonage Communications APIs (formerly Nexmo), Plivo, Podium, Telnyx, <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Zipwhip, CometChat, MessageBird.  <br />
+              &nbsp;&nbsp;&nbsp;&nbsp;-GraphQL, Apollo <br />
+              &nbsp;&nbsp; DevOps operations:  <br />
+              &nbsp;&nbsp;&nbsp;&nbsp;- Github, Gitlab, Bitbucket, AWS, Github actions, Trello, Jenkins, AWS S3. <br />
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-thumbnail circle" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ONLINE</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links w-50">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/coder7290"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://twitter.com/nan-guoxing-0729"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/nan-guoxing-0729/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/nan-guoxing-0729/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
     </section>
   );
 }
