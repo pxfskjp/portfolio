160d2f6055bfd476414cc7911c793038de4b9cdf
coder7290 <topdev0729@gmail.com>
2021-11-09 04:58:33 +0100

    fix: change my projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,12 +3,12 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.jpeg";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import TwilioChat from "../../assets/projects/twilio-chat.gif";
+import CarPlateRead from "../../assets/projects/car-plate.jpg";
+import AndNowUKnow from "../../assets/projects/andnowuknow.jpg";
+import Ustwo from "../../assets/projects/ustwo.com.gif";
+import PhotoEditor from "../../assets/projects/photo-editor.gif";
+import Altasalud from "../../assets/projects/altasalud.com.ar.gif";
 
 function Projects() {
   return (
@@ -24,62 +24,61 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={TwilioChat}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              link="https://github.com/soumyajit4419/Chatify"
+              title="TwilioChat"
+              description="Public and Private chat for the users. Using React js, Node js, Firebase, Twilio API, Implemented Push-Notification."
+              link="#"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={AndNowUKnow}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              link="https://github.com/soumyajit4419/Plant_AI"
+              title="AndNowUKnow"
+              description="AndNowUKnow is a website company dedicated to delivering the latest news, trends and market updates for and about the produce industry"
+              link="#"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={PhotoEditor}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              link="https://github.com/soumyajit4419/Editor.io"
+              title="Photo Editor"
+              description="Photo editor using Angular"
+              link="https://github.com/coder7290/angular-photo-editor"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={Altasalud}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              link="https://github.com/soumyajit4419/Bits-0f-C0de"
+              title="Altasalud.com.ar"
+              description="Alta Salud es el primer edifico de alto estándar en Los Ángeles y la provincia del Bio Bio"
+              link="#"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={Ustwo}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              link="https://github.com/soumyajit4419/AI_For_Social_Good"
+              title="Ustwo.com"
+              description="Ustwo is a global digital product studio launching products, services and companies that make a meaningful impact on the world."
+              link="#"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={CarPlateRead}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              link="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
+              title="Car plate number read amd Face recognize."
+              description="Recognize Car plate from car and read Plate number, and Face detect and blur it. Used tech: OpenCV, Platerecognizer API, YoloFace python packages"
+              link="https://github.com/coder7290/Python-Face-Detection-and-Car-Number-Read"
             />
           </Col>
         </Row>
