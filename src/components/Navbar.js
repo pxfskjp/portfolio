ad4479824c0c8ed68c1b880fe5c841d62d9e7565
Full Stack <nrr0729@gmail.com>
2023-10-02 13:25:17 +0800

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -80,15 +80,15 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            // <Nav.Item>
-            //   <Nav.Link
-            //     as={Link}
-            //     to="/resume"
-            //     onClick={() => updateExpanded(false)}
-            //   >
-            //     <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
-            //   </Nav.Link>
-            // </Nav.Item>
+            {/* <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/resume"
+                onClick={() => updateExpanded(false)}
+              >
+                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
+              </Nav.Link>
+            </Nav.Item> */}
 
             <Nav.Item className="fork-btn">
               <Button
