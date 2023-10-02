2f77318b6aacc69fae305a43d6f817f95c529d04
Full Stack <nrr0729@gmail.com>
2023-10-02 13:05:16 +0800

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { Link } from "react-router-dom";
 import myImg from "../../assets/avatar.png";
 import Tilt from "react-parallax-tilt";
 import {
