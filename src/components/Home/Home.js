4803065b753c12a2bfba8e681cd36366151bc024
Full Stack <nrr0729@gmail.com>
2023-10-02 12:16:58 +0800

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -23,7 +23,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> Nan GuoXing</strong>
+                <strong className="main-name"> Gunther Nico</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
