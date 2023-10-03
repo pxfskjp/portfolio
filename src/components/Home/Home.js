06bf6e62d9fd2f459f563bda20482872a6370036
Full Stack <nrr0729@gmail.com>
2023-10-03 14:16:15 +0800

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 // import myImg from "../../assets/avatar.png";
-import Tilt from "react-parallax-tilt";
+// import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
   AiFillMail,
